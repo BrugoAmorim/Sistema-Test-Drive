1c400b57574edb13b926edce87ffc4896fdf2e87
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-12-20 22:53:27 -0300

    implementando funcao atualizardadoscliente pagesUsuario

--- Sistema/Interface/Pages/PagesUsuario/DetalhesTestDrive/detalhestestdrive.js
+++ Sistema/Interface/Pages/PagesUsuario/DetalhesTestDrive/detalhestestdrive.js
@@ -1,19 +1,30 @@
 
+const nomeusuario = document.getElementById("nm-user");
 window.onload = async () => {
 
-    const id = localStorage.getItem("IDagend");
+    const detalhes = await DetalhesTestDrive();    
+    nomeusuario.appendChild(document.createTextNode(localStorage.getItem("user")));
+    InserirValores(detalhes.dados.cliente, detalhes.dados.datatest);
+    InserirValoresCarro(detalhes.dados.carro);
+}
 
-    const url = "http://localhost:5000/TestDrive/agendamento/detalhes/" + id;
-    const chamaapi = await fetch(url, {
-        mode: 'cors',
-        method: 'GET'
-    });
+const btnSalvar = document.getElementById("btn-salvar-alteracoes");
+btnSalvar.onclick = async () => {
 
-    const res = chamaapi.json();
-    res.then(data => {
+    swal("Você tem certeza que deseja salvar as alterações?", '', 'info', {
+        buttons: {
+            cancel: "Não",
+            catch: {
+                text: "Sim, eu quero continuar",
+                value: "continuar"
+            }
+        }
+    }).then(async value => {
 
-        InserirValores(data.dados.cliente, data.dados.datatest);
-        InserirValoresCarro(data.dados.carro);
+        if(value == "continuar")
+            await atualizarDadosCliente();
+        else
+            swal("Ação cancelada!", '', 'success');
     })
 }
 
@@ -59,4 +70,54 @@ const InserirValoresCarro = (dadoscarro) => {
     Anomodelo.appendChild(document.createTextNode(dadoscarro.anomodelo));
     Anofabricacao.appendChild(document.createTextNode(dadoscarro.anofabricacao));
     Preco.appendChild(document.createTextNode("R$ " + dadoscarro.preco));
+}
+
+const atualizarDadosCliente = async () => {
+
+    const iduser = localStorage.getItem("id");
+    const idcliente = await DetalhesTestDrive();
+
+    const url = "http://localhost:5000/TestDrive/agendamento/atualizarCliente/" + iduser + "/" + idcliente.dados.cliente.idcliente;
+    const dadosClienteReq = {
+ 
+        nomecliente: Nome.value,
+        endereco: Endereco.value,
+        rg: Rg.value,
+        cpf: CPF.value,
+        cnh: CNH.value,
+        telefone: Telefone.value,
+        celular: Celular.value   
+    }
+
+    const chamaapi = await fetch(url, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosClienteReq)
+    });
+
+    const res = chamaapi.json();
+    res.then(data => {
+
+        if(data.codigo == 200)
+            swal(data.status, data.mensagem, 'success')
+        else if(data.codigo == 400)
+            swal(data.status, data.mensagem, 'error')
+    });
+}
+
+const DetalhesTestDrive = async () => {
+
+    const id = localStorage.getItem("IDagend");
+
+    const url = "http://localhost:5000/TestDrive/agendamento/detalhes/" + id;
+    const chamaapi = await fetch(url, {
+        mode: 'cors',
+        method: 'GET'
+    });
+
+    const res = chamaapi.json();
+    return res;
 }

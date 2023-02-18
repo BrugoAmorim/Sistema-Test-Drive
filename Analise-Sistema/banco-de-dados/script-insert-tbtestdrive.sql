790d7ecf88200b3c7f3589ee9def9f557aa0ddf8
BrugoAmorim <brunogomes.concomercial@gmail.com>
2023-02-17 23:07:40 -0300

    subindo scripts-insert tb_clientes e tb_test_drive

---
+++ Analise-Sistema/banco-de-dados/script-insert-tbtestdrive.sql
@@ -0,0 +1,32 @@
+
+use dbtestdrive;
+select * from tb_test_drive;
+
+-- Registros tb_test_drive
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-10 09:30:00', 0, 0, 1, 7);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-10 11:30:00', 0, 0, 2, 5);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-10 18:00:00', 0, 0, 3, 10);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-10 09:30:00', 0, 0, 4, 7);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-10 10:00:00', 0, 0, 5, 17);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-21 12:30:00', 0, 0, 6, 4);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-02-14 08:30:00', 0, 0, 7, 12);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-17 14:00:00', 0, 0, 8, 11);
+
+insert into tb_test_drive(dt_test_drive, bl_desmarcado, bl_realizado, id_cliente, id_carro)
+values('2022-01-17 15:00:00', 0, 0, 9, 14);

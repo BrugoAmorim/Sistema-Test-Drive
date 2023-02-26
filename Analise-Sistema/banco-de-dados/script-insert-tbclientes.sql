187e6c8d0b459f90c6fcbd46e8ec05716c8f2d82
Bruno Gomes <87936511+BrugoAmorim@users.noreply.github.com>
2023-02-26 02:59:22 -0300

    refatorando script

--- Analise-Sistema/banco-de-dados/script-insert-tbclientes.sql
+++ Analise-Sistema/banco-de-dados/script-insert-tbclientes.sql
@@ -5,29 +5,29 @@ select * from tb_clientes;
 -- Registros tb_clientes
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Bruno Gomes", "Rua da alegria", "987.654.311-1", "987.654.321-88", "", "11 9090-1234", 1);
+values("Bruno Gomes", "Rua da alegria", "987.654.311-1", "987.654.321-88", "987.654.321-88", "", "11 9090-1234", 1);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Luisa Santana", "Rua Vera cruz", "987.654.311-1", "987.654.321-88", "", "11 9090-4312", 2);
+values("Luisa Santana", "Rua Vera cruz", "987.654.311-1", "987.654.321-88", "987.654.321-88", "", "11 9090-4312", 2);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Pedro Simão", "Rua coronel antonio", "987.654.311-1", "012.654.990-88", "", "81 9090-1234", 3);
+values("Pedro Simão", "Rua coronel antonio", "987.654.311-1", "012.654.990-88", "987.654.321-88", "", "81 9090-1234", 3);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Fernanda Ferreira", "Av. Pinheiros", "987.654.876-1", "123.654.321-88", "", "11 9090-1234", 4);
+values("Fernanda Ferreira", "Av. Pinheiros", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-1234", 4);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Fernanda Ferreira", "Av. Pinheiros", "987.654.876-1", "123.654.321-88", "", "11 9090-1234", 4);
+values("Fernanda Ferreira", "Av. Pinheiros", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-1234", 4);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "", "11 9090-0909", 5);
+values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-0909", 5);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "", "11 9090-0909", 5);
+values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-0909", 5);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "", "11 9090-0909", 5);
+values("Antonio Carlos", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-0909", 5);
 
 insert into tb_clientes(nm_cliente, ds_endereco, nr_rg, nr_cpf, nr_cnh, nr_telefone, nr_celular, id_usuario)
-values("Luisa Santana", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "", "11 9090-0909", 2);
+values("Luisa Santana", "Av. Atlantica", "987.654.876-1", "123.654.321-88", "987.654.321-88", "", "11 9090-0909", 2);
 
